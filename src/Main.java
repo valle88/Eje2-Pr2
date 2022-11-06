55c0e1bfc09bf254d063441273b8f6f0f785650e
valle <vallukis@gmail.com>
2022-11-06 20:59:27 +0000

    feact: poner lo que se le pide en el main y comprobar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pila mipila = new Pila();
+        int valorUsuario = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero: ");
+        valorUsuario = sc.nextInt();
+        sc.nextLine();
+        mipila.push(valorUsuario);
+
+        System.out.println("Selecione un operador");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+
+        valorUsuario = sc.nextInt();
+
+        int numerador1 = mipila.pop();
+        int numerador2 = mipila.pop();
+        int resultado = 0;
+
+        switch (valorUsuario){
+            case 1:
+                resultado = numerador1 + numerador2;
+                break;
+            case 2:
+                resultado= numerador1 - numerador2;
+                break;
+            case 3:
+                resultado = numerador1 * numerador2;
+                break;
+            case 4:
+                resultado = numerador1 / numerador2;
+                break;
+            default:
+                System.out.println("No ha puesto lo que se le pide");
+                break;
+        }
+        System.out.println("el Resultado de la operacion es: " +resultado);
     }
 }
